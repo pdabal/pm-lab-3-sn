7871fc2bcdee5843c38b475368c26bbb98e02b5e
Łukasz Drozd <lukasz.drozd1@student.wat.edu.pl>
2020-05-23 14:49:26 +0200

    Wykonano zadanie 3.4.2 – T/C1 rejestry

--- Zadanie-3-4/src/main.cpp
+++ Zadanie-3-4/src/main.cpp
@@ -1,20 +1,18 @@
-#include <Arduino.h>
-#define PIN_G 5
-#define PIN_B 6
-#define PIN_R 9
+#include <avr/io.h>
+#include <avr/interrupt.h>
 
-int r, g, b;
-
-void setup()
-{
-  pinMode(PIN_G, OUTPUT);
-  pinMode(PIN_B, OUTPUT);
-  pinMode(PIN_R, OUTPUT);
+int main(void){
+  DDRD &= ~(1<<DDD5);
+  PORTD |= (1 << PORTD5);
+  TIMSK1 |= (1 << TOIE1);
+  TCCR1B |= (1 << CS12) | (1 << CS11) | (1 << CS10);
+  sei();
+  
+  while(1)
+  {
+  }
 }
 
-void loop()
-{
-  analogWrite(PIN_R, rand());
-  analogWrite(PIN_G, rand());
-  analogWrite(PIN_B, rand());
+ISR(TIMER1_OVF_vect){
+
 }
