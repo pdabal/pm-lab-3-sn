0b392a1f8feba1e8464dd73b6631ed734f040b51
„Michal <michal.matuszewski@student.wat.edu.pl>
2020-05-23 14:20:11 +0200

    Wykonano zadanie 3.3.1 – analogRead

--- Zadanie-3-3/src/main.cpp
+++ Zadanie-3-3/src/main.cpp
@@ -1,9 +1,22 @@
 #include <Arduino.h>
-
-void setup() {
-  // put your setup code here, to run once:
+#define ADC_RESOLUTION_DEFAULT (5.0f / 1024.0f)
+uint16_t digital;
+float Voltage;
+void setup()
+{
+Serial.begin(9600);
+analogReference(DEFAULT);
+pinMode(A0, INPUT);  
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() 
+{
+digital = analogRead(A0);
+Voltage = ADC_RESOLUTION_DEFAULT * digital;
+Serial.print("A0 = ");
+Serial.print(digital);
+Serial.print(" \t V(0) = ");
+Serial.print(Voltage);
+Serial.println(" [V] ");
+delay(1000);
 }
