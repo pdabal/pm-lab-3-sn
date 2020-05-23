b126d7952f72534d976e4ff6faffc9928f0428ce
Łukasz Drozd <lukasz.drozd1@student.wat.edu.pl>
2020-05-23 13:52:37 +0200

    Wykonano zadanie 3.3.2 – ADCsingleREAD

--- Zadanie-3-3/src/main.cpp
+++ Zadanie-3-3/src/main.cpp
@@ -1,20 +1,34 @@
 #include <Arduino.h>
-
 #define ADC_RESOLUTION_DEFAULT (5.0f / 1024.0f)
 
 uint16_t digital;
 float voltage;
 
+uint16_t ADCsingleREAD(uint8_t adcToUse)
+{
+  uint16_t ADCval;
+  
+  ADMUX = adcToUse;
+  ADMUX |= (1 << REFS0);
+  ADMUX &= ~(1 << ADLAR);
+  ADCSRA |= (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
+  ADCSRA |= (1 << ADEN);
+  ADCSRA |= (1 << ADSC);
+  while (ADCSRA & (1 << ADSC))
+    ;
+  ADCval = ADCL;
+  ADCval = (ADCH << 8) + ADCval;
+  
+  return ADCval;
+}
 void setup()
 {
   Serial.begin(9600);
-  analogReference(DEFAULT);
-  pinMode(A0, INPUT);
 }
 
 void loop()
 {
-  digital = analogRead(A0);
+  digital = ADCsingleREAD(0);
   voltage = ADC_RESOLUTION_DEFAULT * digital;
   Serial.print("A0 = ");
   Serial.print(digital);
