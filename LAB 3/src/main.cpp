b4440116c54cf54e044090250d682f2442c75ada
Hubert Zbikowski <hubert.zbikowski01@student.wat.edu.pl>
2020-05-23 14:18:31 +0200

    zadanie 3.3.1

---
+++ LAB 3/src/main.cpp
@@ -0,0 +1,47 @@
+#include <Arduino.h> 
+
+#define ADC_RESOLUTION_DEFAULT (5.0f / 1024.0f) 
+
+ 
+ 
+
+uint16_t digital; 
+
+float voltage; 
+
+ 
+ 
+ 
+
+void setup() { 
+
+ Serial.begin(9600); 
+
+ analogReference(DEFAULT); 
+
+ pinMode (A0, INPUT); 
+
+} 
+
+ 
+ 
+
+void loop() { 
+
+  digital = analogRead(A0); 
+
+  voltage = ADC_RESOLUTION_DEFAULT * digital; 
+
+  Serial.print("A0 = " ); 
+
+  Serial.print(digital); 
+
+  Serial.print(" \t V(0) = "); 
+
+  Serial.print(voltage); 
+
+  Serial.print(" (V)"); 
+
+  delay(1000); 
+
+} 
