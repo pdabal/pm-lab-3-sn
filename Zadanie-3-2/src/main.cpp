0b9eaf1e92f08749a2e3edadfcc39947ec6896d0
Ewelina Rębiś <ewelina.rebis@student.wat.edu.pl>
2020-05-23 14:11:28 +0200

    Wykonano zadanie 3-2-1 - wysyłanie tekstu i liczby

--- Zadanie-3-2/src/main.cpp
+++ Zadanie-3-2/src/main.cpp
@@ -1,36 +1,15 @@
 #include <Arduino.h>
 
-#define BUTTON 2
+uint8_t i=0;
 
-int stanPrzycisku;
-int poprzedniStanPrzycisku = LOW;
-unsigned long poprzedniCzas =0;
-unsigned int liczba_nacisniec =0;
-
-void setup(){
+void setup()
+{
   Serial.begin(9600);
-  pinMode(BUTTON,INPUT);
+  Serial.println("Witaj rogramisto");
 }
 
-void loop()
+void loop ()
 {
-  int odczyt = digitalRead(BUTTON);
-
-  if (odczyt != poprzedniStanPrzycisku)
-  {
-    poprzedniCzas = millis();
-  }
-
-  if ((millis() - poprzedniCzas) > 50){
-    if (odczyt!= stanPrzycisku){
-      stanPrzycisku = odczyt;
-      if (stanPrzycisku == HIGH){
-        liczba_nacisniec++;
-
-        Serial.print("Liczba nacisciec przycisku: ");
-        Serial.println(liczba_nacisniec);
-      }
-    }
-  }
-  poprzedniStanPrzycisku= odczyt;
+  Serial.println(i);
+  delay(2000);
 }
